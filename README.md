bdc464d61fad60d1999ac94460de45554061e29f
Alison Keen <ali.keen@gmail.com>
2016-09-15 11:30:24 +0000

    Initial version of scraper, tweaked from lower house scraper

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
+
+The data in this scraper is incomplete - actual election type and past MPs aren't included in the database yet. (sorry). Maybe one day... or you could write it if you want! 
+
+This parser repeats the scrape of the MP list, and then reads out information for upper house MPs only, into the fields expected by OpenAustralia's parser.
+
+Those two csv files need to be generated separately to get their own URLs, so that forked openaustralia parsers can pull them into a database. 
+
+scraper #1: List of MPS - https://morph.io/alisonkeen/SA\_MHRS\_for\_OA\_input
+fields {id, saparl ID, full name}
+destination: /var/www/openaustralia/openaustralia-parser/data/people.csv
+
+scraper #2: List of MHRs - https://morph.io/alisonkeen/SA\_members\_for\_OA\_parser
+fields {id, id2, Name, Electorate, State/Territory(?), Start date, Election type, End date, reason, Most recent party}
+destination: /var/www/openaustralia/openaustralia-parser/data/representatives.csv
+
+scraper #3: List of Senators - https://morph.io/alisonkeen/SA\_senators\_for\_OA\_parser
+fields {id, id2, Name, Electorate, State/Territory(?), Start date, Election type, End date, reason, Most recent party}
+destination: /var/www/openaustralia/openaustralia-parser/data/senators.csv
+
+Ruby script to read these three .csv files into OpenAustralia: see https://github.com/alisonkeen/openaustralia-parser-sandbox/import-data-from-morph.rb
+
+
 This is a scraper that runs on [Morph](https://morph.io). To get started [see the documentation](https://morph.io/documentation)
--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,105 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+#!/bin/env ruby
+# encoding: utf-8
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+# This script started off as a shamelessly plagiarized version of the Popolo/ EveryPolitician Virgin Islands example scraper (thanks, guys!!)
+# Hopefully it isn't too similar but it gave me a very readable starting point, as a ruby beginner. :) 
+# - Alison
+
+# Original is here: 
+# https://github.com/tmtmtmtm/us-virgin-islands-legislature/blob/master/scraper.rb
+
+require 'scraperwiki'
+require 'nokogiri'
+require 'open-uri'
+
+require 'time' #this allows parsing the start dates etc
+
+# This version uses fake data for start and end dates, because I am trying to get enough of the code up and running that I can start to see a South Australian version of the site ... 
+
+sa_mps_url = 'https://www2.parliament.sa.gov.au/Internet/DesktopModules/Memberlist.aspx'
+
+def noko_for(url)
+  Nokogiri::HTML(open(url).read)
+end
+
+def scrape_list(url)
+  noko = noko_for(url)
+  noko.css('table table table tr').each do |line|
+    party_abbrev = line.css('td')[1].text.to_s
+    if (line.css('td')[2].text.to_s == "LC")
+      line.css('a[href*="MemberDrill"]').each do |a|
+        mp_url = URI.join url, a.attr('href')
+        scrape_person(a.text, mp_url, party_abbrev)
+      end
+    else
+       puts "\n(+ lower house member ignored)"
+    end
+  end
+end
+
+def scrape_person(mp_name, url, which_party)
+
+   noko = noko_for(url)
+
+   #reset vars for each MP
+   electorate_name = nil
+   elected_date = " "
+   
+   puts "\nName: " + mp_name.to_s
+
+   # Iterate over the rows of general info and try and
+   # identify specific information
+   info_rows = noko.css('table table table tr')
+   info_rows.each do |row|
+
+     #Reset variables for each row
+     label = nil, cell_content = nil
+     info_cells = row.css('td[class*="boxedtext"]')
+     next if info_cells[0] == nil  # ignore lines with no "boxedtext"
+
+     label = info_cells[0].text.strip 
+     cell_content = info_cells[1].text.strip 
+
+     # Check if line contains Electorate
+     if label.to_s == "Electorate" 
+        electorate_name = cell_content
+     end
+ 
+
+     # Check if line contains start date
+     if label.to_s == "Date elected" 
+        elected_date_string = cell_content
+        elected_date = Time.parse(elected_date_string).strftime("%d.%m.%Y")
+     end
+
+   end #end row interation
+
+  #conditional assign: if electorate hasn't been found, set to " "
+  electorate_name ||= " "
+
+   data = { 
+     member_count: url.to_s.split("=").last,
+     id__saparl: url.to_s.split("=").last,
+     full_name: mp_name,
+     division: electorate_name,
+     state: 'SA',
+     start_date: elected_date,
+     election_type: ' ',
+     end_date: '', 
+     reason_left: 'still_in_office',
+     party: which_party,
+   }
+
+   puts data.to_s
+
+   data[:image] = URI.join(url, data[:image]).to_s unless data[:image].to_s.empty?
+
+  ScraperWiki.save_sqlite([:member_count], data)
+
+end
+
+scrape_list(sa_mps_url)
+
+#Testing: just try it on one page first... 
+#scrape_person('Frances Bedford', 'https://www2.parliament.sa.gov.au/Internet/DesktopModules/MemberDrill.aspx?pid=543')
